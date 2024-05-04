43898f24d3e9d0bc712be6765d1898ed0efad993
Skrinja <clement.simon@hainaut-promsoc.be>
2024-05-04 15:03:15 +0200

    IMP: Correction bug .tmx et .tsx

--- Application/Modern_Exteriors_Complete_Tileset.tsx
+++ Application/Modern_Exteriors_Complete_Tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Modern_Exteriors_Complete_Tileset" tilewidth="16" tileheight="16" tilecount="49808" columns="176">
- <image source="../../../../Desktop/ASSETS/ASSETS-2D/Exteriors/Modern_Exteriors_16x16/Modern_Exteriors_Complete_Tileset.png" trans="000000" width="2816" height="4528"/>
+ <image source="../Application/Modern_Exteriors_Complete_Tileset.png" trans="000000" width="2816" height="4528"/>
 </tileset>
