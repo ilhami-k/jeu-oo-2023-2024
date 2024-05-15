2b6f81d9b99785482bef78ec085d3ef776693c02
moise mundaya <161621843+Belend98@users.noreply.github.com>
2024-05-15 18:59:21 +0200

    ADD : item - settings
    création des items de la classe Armor et Power

--- Application/item.py
+++ Application/item.py
@@ -25,22 +25,31 @@ class Healer (Item):
 class Power (Item):
     """reprend les amélioration qui auront pour effet 
     changer la qualité de la balle tirée (vitesse, damage, taille)"""
+    def __init__(self, nom, info, scale, color, bullet_speed, bullet_damage, bullet_size):
+        super().__init__(nom, info, scale, color)
+        self.bullet_speed = bullet_speed
+        self.bullet_damage = bullet_damage
+        self.bullet_size = bullet_size
+        
     def damage (self):
         pass
 
 class Armor (Item):
-    """reprend les armures qui créent une deuxieme barre de vie"""
+    """reprend les armures qui augmente la barre de vie principale"""
+    def __init__(self, nom, info, scale, color, shield):
+        super().__init__(nom, info, scale, color)
+        self.shield = shield
     def protect():
         pass
 
 
-appel = Healer('pomme', APPLE_INFO, APPLE_SIZE, APPEL_COLOR, APPLE_HEAL)
-berry = Healer("baie", BERRY_INFO, BERRY_SIZE, BERRY_COLOR, BERRY_HEAL)
-military= Armor("militaire", "armure de grade militaire", (0, 0, 255), (40, 40))
-police = Armor("police", "armure de police", (255, 255, 0), (40, 40))
-uzi = Power("uzi", "arme automatique à courte portée", (255, 0, 255), (30, 15))
-bazooka = Power("bazooka", "arme explosive à longue portée", (0, 255, 255), (40, 20))
-pistol = Power("pistol", "arme de poing standard", (255, 0, 0), (25, 15))
+appel = Healer('pomme', APPLE_INFO, APPLE_SCALE, APPEL_COLOR, APPLE_HEAL)
+berry = Healer("baie", BERRY_INFO, BERRY_SCALE, BERRY_COLOR, BERRY_HEAL)
+military= Armor("militaire", MILITARY_INFO, MILITARY_SCALE, MILITARY_COLOR, MILITARY_SHIELD )
+police = Armor("police", PISTOL_INFO, PISTOL_SCALE, PISTOL_COLOR, MILITARY_SHIELD)
+uzi = Power("uzi", UZI_INFO, UZI_SCALE, UZI_COLOR, UZI_SPEED, UZI_DAMAGE, UZI_SCALE)
+bazooka = Power("bazooka", BAZOOKA_INFO, BAZOOKA_SCALE, BAZOOKA_COLOR, BAZOOKA_SPEED, BAZOOKA_DAMAGE, BAZOOKA_SCALE)
+pistol = Power("pistol", PISTOL_INFO, PISTOL_SCALE, PISTOL_COLOR, PISTOL_SPEED, PISTOL_DAMAGE, PISTOL_SCALE)
 
 
 list_items = [appel, berry, military, police, uzi, bazooka, pistol]
