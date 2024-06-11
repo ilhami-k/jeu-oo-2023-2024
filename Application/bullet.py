f65b9c30195cac472df38e7d6a53d58cb3d38ced
moise mundaya <161621843+Belend98@users.noreply.github.com>
2024-06-11 15:25:25 +0200

    IMP : changement de la forme des balles
    Les balles ont un aspect rond au lieu de carré

--- Application/bullet.py
+++ Application/bullet.py
@@ -3,16 +3,19 @@ import math
 from settings import *
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, x, y, angle, speed, lifetime, damage, scale, color):
+    def __init__(self, x, y, angle, speed, lifetime, damage, radius, color):
         super().__init__()
         self.position = [x, y]  # Convertir la position en liste
         self.angle = angle
         self.speed = speed
         self.lifetime = lifetime
         self.damage = damage
-        self.image = pygame.Surface((scale, scale))
-        self.image.fill(color)  
+        self.radius = radius
+
+        self.image = pygame.Surface((radius*2, radius*2), pygame.SRCALPHA)
+        pygame.draw.circle(self.image, color, (radius, radius), radius)
         self.rect = self.image.get_rect(center=self.position)
+
         
     def update(self):
         self.position[0] += math.cos(self.angle) * self.speed
@@ -27,16 +30,16 @@ class Bullet(pygame.sprite.Sprite):
     
 class BasicBullet(Bullet):
     def __init__(self, x, y, angle):
-        super().__init__(x, y, angle, BULLET_SPEED, BULLET_LIFETIME, BULLET_DAMAGE, BULLET_SCALE, BULLET_COLOR)    
+        super().__init__(x, y, angle, BULLET_SPEED, BULLET_LIFETIME, BULLET_DAMAGE, BULLET_RADIUS, BULLET_COLOR)    
     
 class SuperBullet(Bullet):
     def __init__(self, x, y, angle):
-        super().__init__(x, y, angle, SUPER_BULLET_SPEED, SUPER_BULLET_LIFETIME, SUPER_BULLET_DAMAGE, SUPER_BULLET_SCALE, SUPER_BULLET_COLOR)
+        super().__init__(x, y, angle, SUPER_BULLET_SPEED, SUPER_BULLET_LIFETIME, SUPER_BULLET_DAMAGE, SUPER_BULLET_RADIUS, SUPER_BULLET_COLOR)
 
 class SniperBullet(Bullet):
     def __init__(self, x, y, angle):
-        super().__init__(x, y, angle, SNIPER_BULLET_SPEED, SNIPER_BULLET_LIFETIME, SNIPER_BULLET_DAMAGE, SNIPER_BULLET_SCALE, SNIPER_BULLET_COLOR)
+        super().__init__(x, y, angle, SNIPER_BULLET_SPEED, SNIPER_BULLET_LIFETIME, SNIPER_BULLET_DAMAGE, SNIPER_BULLET_RADIUS, SNIPER_BULLET_COLOR)
 
 class BossBullet(Bullet):
     def __init__(self, x, y, angle):
-        super().__init__(x, y, angle, GOLEM_SHOOT_SPEED, GOLEM_SHOOT_LIFETIME, GOLEM_SHOOT_DAMAGE, GOLEM_SHOOT_SCALE, GOLEM_SHOOT_COLOR)
+        super().__init__(x, y, angle, GOLEM_SHOOT_SPEED, GOLEM_SHOOT_LIFETIME, GOLEM_SHOOT_DAMAGE, GOLEM_SHOOT_RADIUS, GOLEM_SHOOT_COLOR)
